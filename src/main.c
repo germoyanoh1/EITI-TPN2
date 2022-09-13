c6474df11010d00ed60c083afcd44424f44430c8
German Moyano <germoyanoh1@gmail.com>
2022-09-12 23:27:29 -0300

    Borrado de archivos innecesarios y actualizacion de main con las plantillas

--- src/main.c
+++ src/main.c
@@ -1,11 +1,55 @@
-#include "foo.h"
-#include "bar.h"
+/* Copyright 2022, Asignatura de "SISTEMAS EMBEBIDOS"
+ * Postgrado EITI 
+ * Facultad de Ciencias Exactas y Tecnología 
+ * Universidad Nacional de Tucuman
+ * Copyright 2022, Moyano German <germoyanoh1@gmail.com>
+ * All rights reserved.
+ */
+
+/** @file main.c
+ **
+ ** @brief Programa principal del TPn2
+ **
+ ** 
+ ** | RV | YYYY.MM.DD | Autor       | Descripción de los cambios              |
+ ** |----|------------|-------------|-----------------------------------------|
+ ** |  1 | 2022.09.12 | gmoyanoh1   | Version inicial del archivo             |
+ ** 
+ ** @defgroup TP2 Plantillas de Archivos
+ ** @brief Plantillas de archivos normalizadas
+ ** @{ 
+ */
+
+/* === Inclusiones de cabeceras ============================================ */
+#include "main.h"
 #include <stdio.h>
+
+/* === Definicion y Macros privados ======================================== */
+
+/* === Declaraciones de tipos de datos privados ============================ */
+
+/* === Definiciones de variables privadas ================================== */
+
+/* === Definiciones de variables publicas ================================== */
+
+/* === Declaraciones de funciones privadas ================================= */
+
+/* === Definiciones de funciones privadas ================================== */
+
+/* === Definiciones de funciones publicas ================================== */
 
 int main(void) {
 	printf("Empieza el programa principal\r\n");
-	foo();
-	bar();
+	
 	printf("Termina el programa principal\r\n");
 	return 0;
 }
+
+/* === Ciere de documentacion ============================================== */
+
+/** @} Final de la definición del modulo para doxygen */
+
+
+
+
+
--- src/main.h
+++ src/main.h
@@ -1,6 +1,49 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+/* Copyright 2022, Asignatura de "SISTEMAS EMBEBIDOS"
+ * Postgrado EITI 
+ * Facultad de Ciencias Exactas y Tecnología 
+ * Universidad Nacional de Tucuman
+ * Copyright 2022, Moyano German <germoyanoh1@gmail.com>
+ * All rights reserved.
+ */
+
+#ifndef MAIN_H   /*! @cond    */
+#define MAIN_H   /*! @endcond */
+
+/** @file main.h
+ **
+ ** @brief Declaraciones del programa principal del TPn2 
+ ** 
+ ** | RV | YYYY.MM.DD | Autor       | Descripción de los cambios              |
+ ** |----|------------|-------------|-----------------------------------------|
+ ** |  1 | 2022.09.12 | gmoyanoh1   | Version inicial del archivo             |
+ ** 
+ ** @defgroup plantilla Plantilals de Archivos
+ ** @brief Plantillas de archivos normalizadas
+ ** @{ 
+ */
+
+/* === Inclusiones de archivos externos ==================================== */
+
+/* === Cabecera C++ ======================================================== */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Definicion y Macros publicos ======================================== */
+
+/* == Declaraciones de tipos de datos publicos ============================= */
+
+/* === Declaraciones de variables publicas ================================= */
+
+/* === Declaraciones de funciones publicas ================================= */
 
 int main(void);
 
+/* === Ciere de documentacion ============================================== */
+#ifdef __cplusplus
+}
 #endif
+
+/** @} Final de la definición del modulo para doxygen */
+
+#endif   /* MAIN_H */
